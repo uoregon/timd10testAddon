500600182fc1f2fe7bf91023a24e0c521491cc7a
Tim Louie <timlouie@uoregon.edu>
2023-11-08 17:51:56 -0800

    more testing

--- upstream-configuration/scripts/UpstreamChange.php
+++ upstream-configuration/scripts/UpstreamChange.php
@@ -1,28 +1,5 @@
 <?php
-
-/**
- * @file
- * Contains \DrupalComposerManaged\ComposerScripts.
- *
- * Custom Composer scripts and implementations of Composer hooks.
- */
-
-namespace UoDrupalComposer;
-
-use Composer\IO\IOInterface;
-use Composer\Script\Event;
-use Composer\Util\Filesystem;
-use Composer\Util\ProcessExecutor;
-
-/**
- * Implementation for Composer scripts and Composer hooks.
- */
-class UpstreamChange {
-
-    public static function changeUpstream(Event $event) {
         chdir(getenv('HOME') . '/code');
 
         print "\n====== Running 'Tim composer require' ======\n\n";
         passthru("composer require 'drupal/pantheon_secrets:^1.0'  2>&1");
-      }
-}
